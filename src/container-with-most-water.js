570a4205833c2defb753223a2d9445e527686379
srsoumya96 <57213758+srsoumya96@users.noreply.github.com>
2023-11-27 12:01:04 +0530

    Create container-with-most-water.js

---
+++ src/container-with-most-water.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/container-with-most-water
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let max = 0;
+    let left = 0, right = height.length-1;
+    while(left < right) {
+        max = Math.max(max, Math.min(height[left], height[right]) * (right-left));
+        if(height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return max;
+};
